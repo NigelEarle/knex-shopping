e136772a37b660729832343c7950055bfc2fa61b
nwre87 <nwre87@gmail.com>
2017-12-05 12:23:17 -1000

    stub routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const users = require('./routes/users')
+const products = require('./routes/products')
+const cart = require('./routes/cart')
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/users', users);
+app.use('/products', products);
+app.use('/cart', cart);
 
 
 app.listen(PORT, (err) => {
