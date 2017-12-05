a86a56f1ff76612f428a43b5bc30af8c43a2a3ef
nwre87 <nwre87@gmail.com>
2017-12-05 13:11:35 -1000

    remove extra id primary key

--- knex/migrations/20171205121327_create_cart_table.js
+++ knex/migrations/20171205121327_create_cart_table.js
@@ -1,11 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('cart', function(table) {
     table.increments();
-    table.increments('id').primary();
     table.integer('user_id').references('users.id');
-    // table.integer('user_id').references('id').inTable('users');
     table.integer('product_id').references('products.id');
-    // table.integer('product_id').references('id').inTable('products');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   })  
 };
