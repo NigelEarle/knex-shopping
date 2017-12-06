51da12b366adf39f7f398815a6a28fa2559d53cf
nwre87 <nwre87@gmail.com>
2017-12-05 16:10:27 -1000

    new product route

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,19 @@ const express = require('express');
 const knex = require('../knex/knex');
 const router = express.Router();
 
+function validateProduct(req, res, next) {
+  const {
+    title,
+    description,
+    inventory,
+    price
+  } = req.body
+  if (!title || !description || !inventory || !price) {
+    return res.json({ message: 'Must send all fields' })
+  }
+  return next();
+}
+
 router.get('/', (req, res) => {
   // fetch all products
   return knex.select('*').table('products')
@@ -19,15 +32,45 @@ router.get('/:product_id', (req, res) => {
 
   return knex.where('id', product_id).table('products')
   .then(product => {
-    return res.json(product);
+    if (product.length > 0) {
+      // product found
+      return res.json(product);
+    }
+    return res.json({ message: 'Product not found' })
   })
   .catch(err => {
     return res.json(err);
   })
 });
 
-router.post('/new', (req, res) => {
- // create new product
+router.post('/new', validateProduct, (req, res) => {
+  // create new product
+  const {
+    title,
+    description,
+    price,
+    inventory
+  } = req.body;
+
+  return knex.insert({
+    title: title,
+    description: description,
+    price: price,
+    inventory: inventory
+  })
+  .into('products')
+  .then(result => {
+    if (result.rows.length === 0) {
+      // check result.rows obj, insert doesn't return object
+      return knex.where('title', title).table('products')
+    }
+  })
+  .then(product => {
+    return res.json(product);
+  })
+  .catch(err => {
+    return res.json(err);
+  })
 });
 
 router.put('/:product_id/update', (req, res) => {
