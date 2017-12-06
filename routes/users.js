5e57190ae7beefb363a447cb77d2f2cbf529ec39
nwre87 <nwre87@gmail.com>
2017-12-05 15:09:35 -1000

    users register route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,15 @@ const express = require('express');
 const knex = require('../knex/knex');
 const router = express.Router();
 
+function validateCredentials(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.send('Must send email and password');
+  }
+  return next();
+}
+
 router.get('/', (req, res) => {
   // fetch all users
   return knex.select().table('users')
@@ -27,12 +36,31 @@ router.get('/:user_id', (req, res) => {
   })
 });
 
-router.post('/login', (req,res) => {
+router.post('/login', validateCredentials, (req,res) => {
   // test creds in req.body against finding user by id
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', validateCredentials, (req, res) => {
   // register new user in db
+  const { email, password } = req.body;
+
+  return knex.select().where('email', email).table('users')
+  .then(user => {
+    if (user.length > 0){
+      // user found, will not insert
+      return res.json({ message: 'User already exists' })
+    }
+    return knex.insert({ email: email, password: password}).into('users');
+  })
+  .then(result => {
+    return knex('users').where('email', email); // fetch user because insert doesn't return user obj.
+  })
+  .then(user => {
+    return res.json(user);
+  })
+  .catch(err => {
+    return res.json(err);
+  });
 });
 
 router.put('/:user_id/forgot-password', (req, res) => {
